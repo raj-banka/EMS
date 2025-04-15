95967f72aa1e7422e4e5e9218ccc5f4f570e4569
raj-banka <rajbanka80@gmail.com>
2025-04-15 13:03:23 +0530

    add eye functionality

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 function Login() {
     const [email, setEmail] = useState("");
     const [ password, setPassword] = useState("");
+    const [showPassword, setShowPassword] = useState(false);
 
     const submitHandler = (e)=>{
         e.preventDefault();
@@ -15,25 +16,36 @@ function Login() {
   return (
     <div className="flex justify-center items-center h-screen w-screen">
       <div className="border-2 border-emerald-600 rounded-2xl p-20">
-        <form className="flex flex-col items-center justify-center">
+        <form 
+        onSubmit={submitHandler} 
+        className="flex flex-col items-center justify-center">
           <input
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          required
             type="email"
             placeholder="Email"
-            required
             className="text-white outline-none bg-transparent border-2 rounded-full border-emerald-600 text-xl px-5 py-3 placeholder:text-gray-400"
           />
+          <div className="relative">
           <input
           value={password}
           onChange={(e) => setPassword(e.target.value)}
-            type="password"
+            type={showPassword ? "text" : "password"}
             placeholder="Enter your password"
             required
-            className="text-white outline-none bg-transparent border-2 rounded-full border-emerald-600 text-xl mt-2 px-5 py-3 placeholder:text-gray-400"
+            className="text-white w-full outline-none bg-transparent border-2 rounded-full border-emerald-600 text-xl mt-2 px-5 py-3 placeholder:text-gray-400"
           />
+          <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className=" absolute right-5 top-3/5 -translate-y-1/2 text-sm text-emerald-300 hover:text-white"
+            > {showPassword ? "Hide" : "Show"}</button>
+            </div>
+            
+            
+            
           <button 
-          onClick={(e) => submitHandler(e)}
           className=" mt-2 text-black outline-none bg-emerald-700  rounded-full text-xl px-5 py-3">
             LogIn
           </button>
